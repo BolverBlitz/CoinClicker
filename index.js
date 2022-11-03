ce2c2cff07731893ed0f69531564b51d7f47acdb
BolverBlitz <bolverblitz@ebg.pw>
2022-11-03 01:17:49 +0100

    Made it easyer for people that have no clue about the internet

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const port = process.env.PORT || 8000;
 setTimeout(() => {
   const app = require('./src/app');
   const { log } = require('./lib/logger');
+
+  // Default to http for relaive URLs
+  if (process.env.controlpanel_url.indexOf('http://') === 0 || process.env.controlpanel_url.indexOf('https://') === 0) {
+    log.system('URL Check passed');
+  }else{
+    process.env.controlpanel_url = 'http://' + process.env.controlpanel_url;
+    log.error('Absolute URL fix applied, this will use http as a fallback!');
+  }
+
+  // Append a / to the end of the controlpanel url if it dosnt have one
+  if (!process.env.controlpanel_url.endsWith('/')) {
+    process.env.controlpanel_url = process.env.controlpanel_url + '/';
+    log.error('URL fix applied');
+  }
+
   setTimeout(() => {
     if (process.env.ExtraErrorWebDelay > 0) {
       log.system(`Webserver was delayed by ${process.env.ExtraErrorWebDelay || 500}ms beause of a error.`);
